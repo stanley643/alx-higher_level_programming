d78bbd381f8d46ad76399715bdee67b42831974e
stanley643 <nmurigi@kabarak.ac.ke>
2024-01-09 01:16:07 -0800

    code a code

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """add two number
+
+    Args:
+        a (int): first value
+        b (int): second value
+
+    Returns:
+        int: sum of a and b
+    """
+    return (a + b)
