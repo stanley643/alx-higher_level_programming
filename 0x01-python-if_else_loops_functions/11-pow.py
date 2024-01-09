53dd08ab460a121801e75d2d90161857e9ddd7be
stanley643 <nmurigi@kabarak.ac.ke>
2024-01-09 01:18:37 -0800

    code a code

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    """computes a to the power of b
+
+    Args:
+        a (int): base
+        b (int): index
+
+    Returns:
+        int: the value of a to the power of b
+    """
+    return (a ** b)
