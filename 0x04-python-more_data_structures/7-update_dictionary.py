143b78964fc18d66957eef8e784c05c4fdf1fb71
stanley643 <nmurigi@kabarak.ac.ke>
2024-01-10 05:20:25 -0800

    code a code

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,12 +2,6 @@
 
 def update_dictionary(a_dictionary, key, value):
     new_dict = a_dictionary.copy()
-    if key in new_dict:
-        new_dict[key] = value
-
-    else:
-        new_dict[key] = value
-
-    print(new_dict)
+    new_dict[key] = value
 
     return new_dict
