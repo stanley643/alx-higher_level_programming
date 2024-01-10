7344412fd2930e76c70cc95bb2a864e84da7c332
stanley643 <nmurigi@kabarak.ac.ke>
2024-01-10 04:01:23 -0800

    code a code

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,10 @@
 
 def search_replace(my_list, search, replace):
     i=0
-    while i < len(my_list):
-        if my_list[i] == search:
-            my_list[i] = replace
+    new_list = my_list.copy()
+    while i < len(new_list):
+        if new_list[i] == search:
+            new_list[i] = replace
+
         i+=1
+    return new_list
