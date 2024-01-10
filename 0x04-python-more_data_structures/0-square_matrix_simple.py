2ffdfa3870ab53c2aaac54e8d31bcee3a9452f32
stanley643 <nmurigi@kabarak.ac.ke>
2024-01-10 03:22:46 -0800

    code a code

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
+    new_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+   # new_matrix = [[x][y]]
     for x in range(len(matrix)):
         for y in range(len(matrix[x])):
-            matrix[x][y] = matrix[x][y]**2
+            new_matrix[x][y] = matrix[x][y]**2
+
+    return new_matrix
 
