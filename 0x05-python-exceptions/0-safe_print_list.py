c5c2fd75fb8375e56df0624091c2afa15108798e
stanley643 <nmurigi@kabarak.ac.ke>
2024-01-23 02:05:29 -0800

    code a code

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except IndexError:
+        pass  # Ignore the IndexError when x is greater than the length of my_list
+    finally:
+        print()
+        return count
