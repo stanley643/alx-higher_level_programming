a733bed70d27b3b9fcdb97def1a312e634611c0f
stanley643 <nmurigi@kabarak.ac.ke>
2024-02-08 01:18:56 -0800

    code a code

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""module to create a square
+
+This module creates a simple square.
+
+Attributes:
+    size: length of each side of the square
+"""
+
+
+class Square:
+    """Represents a square shape
+
+    Attributes:
+        __size (int): length each side of the square.
+    """
+
+    def __init__(self, size):
+        self.__size = size
