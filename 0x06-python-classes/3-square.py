33d6280074eef10b16a7a4242cda7a15ffe4a6d3
stanley643 <nmurigi@kabarak.ac.ke>
2024-02-08 01:23:29 -0800

    code a code

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""module to create a square
+
+    This module creates a simple square given a size
+
+    Attributes:
+    size: length of each side of the square
+"""
+
+
+class Square:
+    """Represents a square shape
+
+    Attributes:
+        __size (int): length each side of the square.
+    """
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """public instance method that returns square area
+
+        Returns:
+            int: the size^2
+        """
+        return (pow(self.__size, 2))
