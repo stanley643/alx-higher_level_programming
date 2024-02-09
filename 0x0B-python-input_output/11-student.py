2ffbe5b7ea2142e86973e2818535ba3de1ea9581
stanley643 <nmurigi@kabarak.ac.ke>
2024-02-09 02:43:48 -0800

    code a code

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Student with a to_json method"""
+
+
+class Student:
+    """Student blueprint
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a list of specific attributes
+
+        Args:
+            attrs (list, optional): list of strings. Defaults to None.
+
+        Returns:
+            dict: dictionary description with specified attributes
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            specific_dict = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    specific_dict[attr] = self.__dict__[attr]
+            return specific_dict
+
+    def reload_from_json(self, json):
+        """reads a dictionary in json format in a file
+
+        Args:
+            json (file): the file containing the json
+        """
+        for x in json:
+            self.__dict__.update({x: json[x]})
