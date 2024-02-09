524184994683d1f9326c4014475431b0a15cae60
stanley643 <nmurigi@kabarak.ac.ke>
2024-02-09 02:30:36 -0800

    code a code

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""simple square base on Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """simple square based on the Rectangle class
+
+    Args:
+        Rectangle (BaseGeometry): parent class
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """computes square area
+
+        Returns:
+            int: size ** 2
+        """
+        return (pow(self.__size, 2))
+
+    def __repr__(self):
+        """returns an ambiguos represantation"""
+        return f"[Square] {self.__size}/{self.__size}"
