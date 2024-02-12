bb17a7ca55730e65ead83c9fbb58bf258bba8d9d
stanley643 <nmurigi@kabarak.ac.ke>
2024-02-12 01:39:45 -0800

    code a code

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,6 +9,6 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            __nb_objects += 1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
