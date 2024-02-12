78f73e5596e8b1a09c900f9d8d53e223318acdc8
stanley643 <nmurigi@kabarak.ac.ke>
2024-02-12 01:30:48 -0800

    code a code

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,4 +3,14 @@
 
 
 class Base:
-    #pass
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects ++
+            id = __nb_objects
+
+if __name__ == "__main__":
+    __init__()
