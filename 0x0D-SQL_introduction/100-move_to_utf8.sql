b84d77539f26c2cbb8925fd0ba835b0a08c22ecc
stanley643 <nmurigi@kabarak.ac.ke>
2024-03-12 17:32:19 -0700

    code a code

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,9 @@
+-- Convert hbtn_0c_0 database, first_table table, and name field to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+-- Convert field name in table first_table to UTF8
+-- Convert database hbtn_0c_0 to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
