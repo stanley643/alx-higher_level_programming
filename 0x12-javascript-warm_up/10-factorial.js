9de7dc3cbbd968b5073dac8e8e5af8c5d45a5b12
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-08 19:28:40 -0700

    code a code

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function factorial(n) {
+    if (isNaN(n) || n === 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
+
