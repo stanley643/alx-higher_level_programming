e298fe3dde37ae9ef1761930ea7e968e9f2e674d
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-10 12:31:00 -0700

    js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,7 @@
 class Rectangle {
     constructor(w, h) {
         if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-            return {}; // Return an empty object if width or height is invalid
+            return {}; //return an empty object if width or height is invalid
         }
         this.width = w;
         this.height = h;
