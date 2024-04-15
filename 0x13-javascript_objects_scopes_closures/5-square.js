8ca8d136463b7af3115d7e85071606460cb806dd
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-15 04:14:22 -0700

    js

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,42 @@
+#!/usr/bin/node
+
+class Rectangle {
+    constructor(w, h) {
+        if (w <= 0 || h <= 0) {
+            return; // Create an empty object if w or h is not a positive integer
+        }
+        this.width = w;
+        this.height = h;
+    }
+
+    print() {
+        if (!this.width || !this.height) {
+            return; // If width or height is not set, return
+        }
+        for (let i = 0; i < this.height; i++) {
+            console.log("X".repeat(this.width));
+        }
+    }
+
+    rotate() {
+        if (!this.width || !this.height) {
+            return; // If width or height is not set, return
+        }
+        [this.width, this.height] = [this.height, this.width];
+    }
+
+    double() {
+        if (!this.width || !this.height) {
+            return; // If width or height is not set, return
+        }
+        this.width *= 2;
+        this.height *= 2;
+    }
+}
+
+class Square extends Rectangle {
+    constructor(size) {
+        super(size, size); // Call the constructor of Rectangle with size as both width and height
+    }
+}
+module.export = Square
