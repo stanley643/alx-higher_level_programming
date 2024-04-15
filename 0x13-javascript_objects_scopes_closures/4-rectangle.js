558232a4687fa105bae1379f927b863b354c6d2c
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-15 04:07:53 -0700

    js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+
+class Rectangle {
+    constructor(w, h) {
+        if (w <= 0 || h <= 0) {
+            return; // Create an empty object if w or h is not a positive integer
+        }
+        this.width = w;
+        this.height = h;
+    }
+
+    print() {
+        if (!this.width || !this.height) {
+            return; // If width or height is not set, return
+        }
+        for (let i = 0; i < this.height; i++) {
+            console.log("X".repeat(this.width));
+        }
+    }
+
+    rotate() {
+        if (!this.width || !this.height) {
+            return; // If width or height is not set, return
+        }
+        [this.width, this.height] = [this.height, this.width];
+    }
+
+    double() {
+        if (!this.width || !this.height) {
+            return; // If width or height is not set, return
+        }
+        this.width *= 2;
+        this.height *= 2;
+    }
+}
+
+module.exports = Rectangle
