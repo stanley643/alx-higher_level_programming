0c764cadac7a1abbc4470a237c7cab7a2ee6d338
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-25 12:13:07 -0700

    wc

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# This script takes in a URL and displays all HTTP methods the server will accept.
-# It uses curl with the -s option to send a silent request and the -I option to fetch only the header of the response.
-# It then extracts the Allow header from the response headers to get the list of accepted HTTP methods.
-
+# This script takes in a URL and displays all HTTP methods 
 curl -sI "$1" | grep -iE '^Allow:' | awk '{print substr($0, 8)}' | tr -d '\r'
-
