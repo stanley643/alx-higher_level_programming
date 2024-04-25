6999319e32fd386f9533e14cb51a2edf6a67e251
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-25 12:08:01 -0700

    wc

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a GET request to the URL, and displays the body of the response.
-# It uses curl with the -s option to send a silent request and only displays the body for a 200 status code response.
-
-curl -s -X GET "$1" -o /tmp/body_response
-if [ $? -eq 0 ]; then
-    cat /tmp/body_response
-fi
-
+# This script takes in a URL, sends a GET request to the URL
+curl -s -X GET "$1" -o /tmp/body_response && cat /tmp/body_response
