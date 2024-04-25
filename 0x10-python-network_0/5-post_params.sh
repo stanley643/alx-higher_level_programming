ae89642bc865e9a9f1bbda5080eb1145bb970dc5
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-25 12:16:48 -0700

    wc

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# This script takes in a URL as an argument, sends a POST request to the URL with specified parameters, and displays the body of the response.
-# It uses curl with the -s option to send a silent request.
-
+# This script takes in a URL as an argument, sends a POST request to the URL with specified parameters
 curl -s -X POST "$1" -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
-
