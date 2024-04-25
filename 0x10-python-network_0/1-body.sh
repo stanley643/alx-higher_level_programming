a61a681133de9f01bd6e30590be35cadd8e39dee
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-25 13:18:01 -0700

    wc

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL
-response=$(curl -s -w "%{http_code}" -X GET "$1"); [ "${response: -3}" = "200" ] && echo "$response" | sed '$d'
+response=$(curl -s -w "%{http_code}" -X GET "$1") && [ "${response: -3}" = "200" ] && echo "$response" | sed '$d'
