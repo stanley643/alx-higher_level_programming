75ce24a8d78b11af1ebd3d64a08ad87b0ae81e6f
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-25 13:15:23 -0700

    wc

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL
-curl -s -X GET "$1" -o /tmp/body_response && cat /tmp/body_response
+response=$(curl -s -w "%{http_code}" -X GET "$1"); [ "${response: -3}" = "200" ] && echo "$response" | sed '$d'
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # This script makes a request to 0.0.0.0:5000/catch_me
-curl -s -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin:HolbertonSchool"
+#!/bin/bash
+curl -s -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin:HolbertonSchool" -L
