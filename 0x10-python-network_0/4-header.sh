6524971ca23a948eec1af313f831414e1ee4d74a
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-25 12:15:09 -0700

    wc

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# This script takes in a URL as an argument, sends a GET request to the URL with a custom header X-School-User-Id set to 98, and displays the body of the response.
-# It uses curl with the -s option to send a silent request.
-
+# This script takes in a URL as an argument, sends a GET request to the URL
 curl -s -H "X-School-User-Id: 98" "$1"
-
