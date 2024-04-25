7fb26212b9c8516e94efb3cb52f38b24239142da
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-25 13:02:14 -0700

    wc

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# This script makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing "You got me!" in the body of the response.
-# It uses curl with the -s option to send a silent request and the -X option to specify the HTTP method.
-
+# This script makes a request to 0.0.0.0:5000/catch_me
 curl -s -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin:HolbertonSchool"
-
