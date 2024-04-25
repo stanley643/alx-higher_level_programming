88b997a2f3426ef48ada5040ae10a65d2ef5d271
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-25 12:59:39 -0700

    wc

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# This script sends a JSON POST request to a URL passed as the first argument and displays the body of the response.
-# It reads the contents of a JSON file passed as the second argument and sends it in the body of the request.
-# It uses curl with the -s option to send a silent request.
-
-if [ -f "$2" ]; then
-    curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
-else
-    echo "Not a valid JSON"
-fi
-
+# This script sends a JSON POST request to a URL passed as the first argument
+if [ -f "$2" ]; then curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"; else echo "Not a valid JSON"; fi
