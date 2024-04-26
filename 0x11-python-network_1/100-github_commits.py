6d995a6f2adb13139844a30bb3b9707a0a47fb4e
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-26 12:51:23 -0700

    github

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module: 100-github_commits
+Fetches and lists the 10 most recent commits of a repository from the GitHub API.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    repository = sys.argv[1]
+    owner = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner}/{repository}/commits"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        commits = response.json()[:10]
+        for commit in commits:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
+    else:
+        print("Error:", response.status_code)
+
