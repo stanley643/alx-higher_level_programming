b03dc11a7c725972963f6330a2a36c0dde20c185
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-26 12:35:08 -0700

    json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module: 8-json_api
+Sends a POST request to search_user with a letter as a parameter.
+Displays the id and name from the response if it's properly JSON formatted and not empty.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    url = 'http://0.0.0.0:5000/search_user'
+    data = {'q': q}
+
+    response = requests.post(url, data=data)
+
+    try:
+        json_data = response.json()
+        if json_data:
+            print("[{}] {}".format(json_data.get('id'), json_data.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+
